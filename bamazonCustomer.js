864bd5c440e9f295b3a148067e372ae82b62841b
Connor Perez <connorperez@gmail.com>
2019-06-04 13:34:22 -0500

    Working except for subtraction

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,13 +3,69 @@ var inquirer = require('inquirer');
 
 var connection = mysql.createConnection({
     host:"localhost",
-    port:3000,
+    port:3306,
     user:"root",
-    password:"Pgk9jhx!",
+    password:"P2gk9jhx!",
     database:"bamazon"
 })
 
 connection.connect(function(err){
     if (err) throw err;
     console.log("connection successful");
+    makeTable();
 })
+
+var makeTable = function(){
+    connection.query("SELECT * FROM products", function(err,res){
+        for (var i = 0; i < res.length; i++){
+            console.log(res[i].itemid+" || " +res[i].productname+" || " +res[i].departmentname+ " || " +res[i].price+ " || " +res[i].stockquantity+"\n");
+        }
+        promptCustomer(res);
+    })
+}
+
+var promptCustomer = function(res){
+    inquirer.prompt([{
+        type:'input',
+        name:'choice',
+        message:"What would you like to purchase?"
+    }]).then(function(answer){
+        var correct = false;
+        if(answer.choice.toUpperCase()=="Q"){
+            process.exit();
+        }
+        for(var i = 0; i < res.length; i++){
+            if (res[i].productname==answer.choice){
+                correct=true;
+                var product=answer.choice;
+                var id=i;
+                inquirer.prompt({
+                    type:"input",
+                    name:"quant",
+                    message:"How many would you like to buy?",
+                    validate: function(value){
+                        if(isNaN(value)==false){
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    }
+                }).then(function(answer){
+                    if ((res[id].stockquantity-answer.quant)>0){
+                        connection.query("UPDATE prodcuts SET stockquantity='"+(res[id].stockquantity-answer.quant)+"'", function(err,res2){
+                            console.log("Product Bought");
+                            makeTable();
+                        })
+                    } else {
+                        console.log("Not a valid selection");
+                        promptCustomer(res);
+                    }
+                })
+            }
+        }
+        if(i==res.length && correct==false){
+            console.log("Not a valid selection");
+            promptCustomer(res);
+        }
+    })
+}
